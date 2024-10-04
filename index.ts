e5ccabe8ff54551d50365370c6009b6a41d82749
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-04 13:48:18 +0200

    Changed string type

--- index.ts
+++ index.ts
@@ -22,8 +22,9 @@ const reviews = [
 ];
 
 // Solution
-function showReviewTotal(value: number) {
-  reviewTotalDisplay.innerHTML = "review total " + value.toString();
+function showReviewTotal(value: number, reviewer: string) {
+  reviewTotalDisplay.innerHTML =
+    "review total " + value.toString() + "| last reviewed by " + reviewer;
 }
 
-showReviewTotal(reviews.length);
+showReviewTotal(reviews.length, reviews[0].name);
