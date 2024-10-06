0d49b350022d59b6e94a67b202e12f14fbffebec
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-06 02:14:49 +0200

    added images and completed tuples challange

--- index.ts
+++ index.ts
@@ -1,64 +1,25 @@
-import { showReviewTotal, populateUser } from "./utils.ts";
-let isOpen: boolean;
+const reviewTotalDisplay = document.querySelector("#reviews");
+const returningUserDisplay = document.querySelector("#returning-user");
+const userNameDisplay = document.querySelector("#user");
 
-// Reviews
-const reviews: {
-  name: string;
-  stars: number;
-  loyaltyUser: boolean;
-  date: string;
-}[] = [
-  {
-    name: "Sheia",
-    stars: 5,
-    loyaltyUser: true,
-    date: "01-04-2021",
-  },
-  {
-    name: "Andrzej",
-    stars: 3,
-    loyaltyUser: false,
-    date: "28-03-2021",
-  },
-  {
-    name: "Omar",
-    stars: 4,
-    loyaltyUser: true,
-    date: "27-03-2021",
-  },
-];
+export function showReviewTotal(
+  value: number,
+  reviewer: string,
+  isLoyalty: boolean
+) {
+  const iconDisplay = isLoyalty ? "⭐" : "";
+  reviewTotalDisplay.innerHTML =
+    "review total " +
+    value.toString() +
+    "| last reviewed by " +
+    reviewer +
+    " " +
+    iconDisplay;
+}
 
-// User
-const you: {
-  firstName: string;
-  lastName: string;
-  isReturning: boolean;
-  age: number;
-  stayedAt: string[];
-} = {
-  firstName: "Bobby",
-  lastName: "Brown",
-  isReturning: true,
-  age: 35,
-  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
-};
-
-//Properties
-const properties: {
-  image: string;
-  title: string;
-  price: number;
-  location: {
-    firstLine: string;
-    city: string;
-    code: number;
-    country: string;
-  };
-  contact: string;
-  isAvailable: boolean;
-}[] = [{}];
-
-// Functions
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
-
-populateUser(you.isReturning, you.firstName);
+export function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning == true) {
+    returningUserDisplay.innerHTML = "back";
+  }
+  userNameDisplay.innerHTML = userName;
+}
